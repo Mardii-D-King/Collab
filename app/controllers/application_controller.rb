4fdbd56c7f469f4fbbc5665141886cee75255c64
Mardon Bailey <mardon_workspace@yahoo.com>
2016-04-10 20:15:21 +0000

    Added user profile pages, and username to sign_up form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+  #Call the configured parameter
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #Protect the database from users to send data to database or modify anything they dont have acess to.
+  protected
+  def configure_permitted_parameters
+	  devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+	  devise_parameter_sanitizer.for(:sign_in){ |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+	  devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:email, :password, :password_confirmation, :remember_me)}
+   end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ root 'pages#Index'
 #Define pages for Pages	
 
 #Chages the path of the pages
-  get 'home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
 
-  get 'explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
